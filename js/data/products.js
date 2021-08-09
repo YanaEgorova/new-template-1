6a2fbc1e5ead54f1cb7ac37eb8c99af350417831
YanaEgorova <yanaegorova69.3@gmail.com>
2021-08-09 19:28:20 +0300

    +

---
+++ js/data/products.js
@@ -0,0 +1,349 @@
+export const products = [
+    {
+        id: 'product-1',
+        name: 'Pen Organizer',
+        price: 5.95,
+        image: './img/img1.png',
+        description: [
+            {
+                title: 'Easy organizing',
+                text: 'Desktop pen and pencil holder helps keep office supplies neatly stored and easy to access.',
+            },
+            {
+                title: 'Versatile',
+                text: 'Tall, sturdy, and spacious enough to hold a variety of office supplies, including pens, pencils, rulers, scissors, and more.',
+            },
+            {
+                title: 'Convenient',
+                text: 'Helps create a neat, orderly work area at home, in the classroom, or at the office.',
+            },
+            {
+                title: 'Elegant',
+                text: 'Clean lines and a simple profile create an overall attractive appearance.',
+            },
+        ],
+        images: ['./img/img1-1.png', './img/img1-2.png', './img/img1-3.png'],
+    },
+    {
+        id: 'product-2',
+        name: 'SteeL Winged Corkscrew with Removable Foil Cutter',
+        price: 19.99,
+        image: './img/img2.png',
+        description: [
+            {
+                text: 'Non-stick, self-centering screw easily removes any style cork.',
+            },
+            {
+                text: 'Curved, die-cast handles offer comfortable hold.',
+            },
+            {
+                text: 'Easy-to-grip extended body.',
+            },
+            {
+                text: 'Soft non-slip knob turns smoothly.',
+            },
+            {
+                text: 'Unique design removes corks without breakage, no pulling necessary.',
+            },
+            {
+                text: 'To release corks, turn the knob counterclockwise. Do not remove corks by hand.',
+            },
+            {
+                text: 'Removable Foil Cutter snaps into base for convenient storage.',
+            },
+        ],
+        images: ['./img/img2-1.png', './img/img2-2.png', './img/img2-3.png'],
+    },
+    {
+        id: 'product-3',
+        name: 'Effortless Heavy Duty Stapler',
+        price: 24.99,
+        discount: 14.99,
+        image: './img/img3.png',
+        description: [
+            {
+                text: 'Heavy duty stapler that can staple from 10 up to 210 sheets of standard weight paper at a time; include 1000 15/16" heavy-duty staples (Used for 160-210 sheets).',
+            },
+            {
+                text: 'With a built-in, front end jam clearing mechanism that will ensure your stapling more efficient and smooth.',
+            },
+            {
+                text: 'Requires less force to operate than traditional heavy duty stapler, allowing you to staple more sheets with less effort; soft grip ensures comfort while stapling.',
+            },
+            {
+                text: 'All-metal design for exceptional durability; non-slip base keeps stapler in place when leverage is applied.',
+            },
+        ],
+        images: ['./img/img3-1.png', './img/img3-2.png', './img/img3-3.png'],
+    },
+    {
+        id: 'product-4',
+        name: 'Paper Trimmer',
+        price: 35.32,
+        discount: 24.65,
+        image: './img/img4.png',
+        description: [
+            {
+                text: 'Everyday portable paper trimmer with 15-inch guillotine-style, self-sharpening steel blade; ideal for home or office.',
+            },
+            {
+                text: 'Cleanly cuts up to 10 sheets of 20-pound paper at a time; sharp enough to accurately trim cardstock, construction paper, photos, and more.',
+            },
+            {
+                text: 'Guardrail keeps fingers a safe distance from the blade; when not in use, trimmer arm can be secured in place, fully hiding the blade.',
+            },
+            {
+                text: 'Durable plastic base with white 1/2-inch gridlines (0.5 by 0.5 inches) and a printed ruler (in inches and cm) that allow for reliable alignment and precision cuts.',
+            },
+            {
+                text: 'Measures 21.7 by 14 by 2.6 inches; weighs 3.9 pounds.',
+            },
+        ],
+        images: ['./img/img4-1.png', './img/img4-2.png'],
+    },
+    {
+        id: 'product-5',
+        name: 'Laptop Sleeve Case',
+        price: 39.99,
+        discount: 39.97,
+        image: './img/img5.png',
+        description: [
+            {
+                text: 'Form-fitting sleeve with quick top-loading access.',
+            },
+            {
+                text: 'Precise fit for laptops and ultrabooks with a 14.1 inch display.',
+            },
+            {
+                text: 'Slim design allows you to carry the case by itself or in a bag.',
+            },
+            {
+                text: 'Internal Dimensions: 13" x 10" x 0.4" (LxWxH).',
+            },
+            {
+                text: 'External Dimensions: 14.2" x 0.8" x 10.5" (LxWxH).',
+            },
+        ],
+        images: ['./img/img5-1.png', './img/img5-3.png'],
+    },
+    {
+        id: 'product-6',
+        name: 'Vacuum Seal System',
+        price: 43.98,
+        discount: 55.97,
+        image: './img/img6.png',
+        description: [
+            {
+                text: 'Vacuum sealer appliance with selectable modes for wet or dry food.',
+            },
+            {
+                text: 'Effectively vacuum seal pre-made meals, produce, meat, dry goods, and more for preservation and freshness.',
+            },
+            {
+                text: 'Safely preserves frozen meat up to 3 years and refrigerated items for weeks.',
+            },
+            {
+                text: 'Includes air suction hose and 16 foot roller bag; LED process indicator lights.',
+            },
+            {
+                text: 'Built-in cutter creates custom bag sizes for easy storage and waste reduction.',
+            },
+        ],
+        images: ['./img/img6-1.png', './img/img6-2.png'],
+    },
+    {
+        id: 'product-7',
+        name: 'Enameled Cast Iron Oven',
+        price: 49.95,
+        discount: 69.99,
+        image: './img/img7.png',
+        description: [
+            {
+                text: '6 Quart Enameled Cast Iron Dutch Oven.',
+            },
+            {
+                text: 'Broil, braise, bake or roast in the oven up to 500 Degree F.',
+            },
+            {
+                text: 'Sauté, simmer or fry on any stovetop.',
+            },
+            {
+                text: 'Unparalleled in heat retention and even heating.',
+            },
+            {
+                text: 'Smooth glass surface won\'t react to ingredients.',
+            },
+        ],
+        images: ['./img/img7-1.png', './img/img7-2.png', './img/img7-3.png'],
+    },
+    {
+        id: 'product-8',
+        name: 'Compact Multi-Functional Digital Air Fryer',
+        price: 69.99,
+        discount: 69.94,
+        image: './img/img8.png',
+        description: [
+            {
+                text: '1400 watt digital air fryer kitchen appliance for healthier eating and faster cooking results.',
+            },
+            {
+                text: 'Content capacity of 3 liters/3.2 quarts with detachable, non-stick air frying basket.',
+            },
+            {
+                text: 'Timer and automatic shut off features ensure delicious results and safety.',
+            },
+            {
+                text: 'Requires little to no cooking oil to make crispy fried foods without the excess calories.',
+            },
+            {
+                text: 'Fry, bake, grill, and roast with one device.',
+            },
+            {
+                text: 'Handy counter top design with rubber non-slip feet.',
+            },
+            {
+                text: 'LED digital display with 7 cooking pre-set opt.',
+            }
+        ],
+        images: ['./img/img8-1.png', './img/img8-2.png', './img/img8-3.png', './img/img8-4.png'],
+    },
+    {
+        id: 'product-9',
+        name: 'Leather Chair',
+        price: 79.99,
+        discount: 79.93,
+        image: './img/img9.png',
+        description: [
+            {
+                title: 'COMFORTABLE RECEPTION CHAIR',
+                text: 'Modern, luxury design with plush padded seat, back, and armrests upholstered in cozy black SofThread leather.',
+            },
+            {
+                title: 'ERGONOMIC LOUNGE CHAIR',
+                text: 'Integrated comfy padded armrests support your guests upper body.',
+            },
+            {
+                title: 'NO DAMAGE DESIGN',
+                text: 'Wall-saver design prevents this guest chair seat back from damaging surfaces.',
+            },
+            {
+                title: 'DURABLE OFFICE FURNITURE',
+                text: 'This heavy duty chair boasts a sturdy black steel tube frame and 250 pound weight capacity for long-lasting use.',
+            },
+            {
+                title: 'WARRANTY & SUPPORT',
+                text: 'Relax knowing this computer chair is backed by the OFM Limited Warranty.',
+            },
+        ],
+        images: ['./img/img9-1.png', './img/img9-2.png', './img/img9-3.png', './img/img9-4.png'],
+    },
+    {
+        id: 'product-10',
+        name: 'Portable Heater',
+        price: 84.41,
+        discount: 95.50,
+        image: './img/img10.png',
+        description: [
+            {
+                text: 'Classic black portable space heater with adjustable electronic thermostat and easy-to-read LED display (measures 14.5 x 11.4 x 14.7 inches; weighs 14.6 pounds).',
+            },
+            {
+                text: 'Includes over-heat sensor for safety.',
+            },
+            {
+                text: 'Adjustable temperature setting from : 65- 85 ℉.',
+            },
+            {
+                text: 'Four infrared heating elements with 750W and 1500W power settings as well as ECO-Smart setting for energy savings.',
+            },
+            {
+                text: 'Includes remote control (batteries not included).',
+            },
+            {
+                text: 'Can heat rooms up to 1000 sq ft (standard 9 foot ceiling height).',
+            },
+        ],
+        images: ['./img/img10-1.png', './img/img10-2.png', './img/img10-3.jpg', './img/img10-4.jpg'],
+    },
+    {
+        id: 'product-11',
+        name: 'Steel Pots and Pans Set',
+        price: 89.99,
+        discount: 109.91,
+        image: './img/img11.png',
+        description: [
+            {
+                title: 'STEEL COOKWARE',
+                text: 'This set has been improved in every area including the nonstick coating, stay cool handles, exterior finish and durability, including adding diamonds to the surface to ensure the pan doesn’t scratch!',
+            },
+            {
+                title: 'ULTRA NONSTICK',
+                text: 'The nonstick cooking surface is coated 3x, reinforced with ceramic and titanium to deliver the ultimate food release. No oil or butter needed, makes for healthier eating, and provides for hassle free, 1-2-3 cleanup.',
+            },
+            {
+                title: '12 PIECE COOKWARE SET',
+                text: 'This set includes everything you need to outfit a kitchen including an 8.5” skillet, 10.5” frying pan with tempered glass lid, 5 quart stock pot with tempered glass lid, 1.5 quart sauce pan with tempered glass lid, 2.5 quart sauce pan with lid, XL aluminum steamer insert for 5 quart pot, bonus shallow square frying pan with tempered seal tight lid.',
+            },
+            {
+                title: 'DISHWASHER SAFE',
+                text: 'Spend less time with cleanup! After all the cooking and prepping is done, place cookware in the dishwasher for quick and easy cleanup. Induction Compatible.',
+            },
+            {
+                title: 'SUPER DURABLE',
+                text: 'The coating of the pan is durable enough to withstand metal spatulas, spoons, whisks and more, while the aluminum exterior is dense, nonporous, and highly wear-resistant for long lasting performance.',
+            },
+        ],
+    },
+    {
+        id: 'product-12',
+        name: 'Professional Radio',
+        price: 109.00,
+        discount: 110.89,
+        image: './img/img12.png',
+        description: [
+            {
+                title: 'Emergency Radio',
+                text: 'Radios provide reliable communication which is not dependent on satellites and cellular networks that can be the difference between getting experienced help in an emergency and being left on your own when you need assistance.',
+            },
+            {
+                title: 'Antenna Warning Indicator',
+                text: 'Take the guess work out of checking your antenna, a light will automatically illuminate if your system needs to be checked.',
+            },
+            {
+                title: '4 Watt Output',
+                text: 'Giving you the best range of communication with the maximum power allowed by law.',
+            },
+            {
+                title: 'Instant Channel 9',
+                text: 'Immediate access to Emergency channel 9 to ensure you\'re driving safely from the very beginning.',
+            },
+            {
+                title: '40 Channel Access',
+                text: 'Scan the complete range of 40 channels for full coverage of CB stations.',
+            },
+        ],
+        images: ['./img/img12-1.jpg', './img/img12-2.jpg'],
+    },
+    {
+        id: 'product-13',
+        name: 'Desktop External Hard Drive',
+        price: 119.00,
+        discount: 120.88,
+        image: './img/img13.png',
+        description: [
+            {
+                text: 'Auto backup with included WD Backup software and Time Machine compatibility.',
+            },
+            {
+                text: 'Password protection with hardware encryption.',
+            },
+            {
+                text: 'Trusted storage built with WD reliability.',
+            },
+            {
+                text: 'USB 3.0 port; USB 2.0 compatible.',
+            },
+        ],
+        images: ['./img/img13-1.png', './img/img13-2.png'],
+    },
+]
